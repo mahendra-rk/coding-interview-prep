c63ae74e021facad6c5a51726f845d573f02eaf2
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2020-10-20 16:20:35 +0530

    adding balanced bracket solution

---
+++ algoexpert/balancedBrackets.py
@@ -0,0 +1,13 @@
+def balancedBrackets(string):
+    pairs = {')':'(', '}':'{', ']':'['}
+    stack = []
+    for item in string:
+    	if item in pairs.keys(): # check if closing bracket
+    		if stack and stack[-1] == pairs[item]: # check if last item in stack is bracket pair ith current bracket
+    			stack.pop()
+    		else:
+    			return False
+    	elif item in pairs.values(): # append opening bracket to stack
+    		stack.append(item) 
+    return not stack
+    
