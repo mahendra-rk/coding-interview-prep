c48b0b0268d4987605b05b4b35885af7e88a8111
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2021-01-07 14:08:47 +0530

    Validate BST

---
+++ algoexpert/validateBST.py
@@ -0,0 +1,15 @@
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def validateBst(tree, min_value=float("-inf"), max_value=float("inf")):
+	if tree is None:
+		return True
+    value = tree.value
+	if value < min_value or value >= max_value :
+		return False
+	return validateBst(tree.left, min_value, value) and validateBst(tree.right, value, max_value)
