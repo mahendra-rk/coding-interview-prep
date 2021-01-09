6f445070800cf505402b46cf4b5c35a9be8d24ae
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2021-01-09 18:54:29 +0530

    adding isPalindrome sol

---
+++ algoexpert/isPalindrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(string):
+    left = 0 
+	right = len(string)-1
+	while left <= right:
+		if string[left] == string[right]:
+			left +=1
+			right -=1
+		else:
+			return False
+	return True
