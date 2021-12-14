0424aa5aa52cd29c049b70bb1388be01f66ff6c2
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2021-12-14 20:10:30 +0530

    #242

---
+++ leetcode/ValidAnagram.py
@@ -0,0 +1,10 @@
+# Leetcode
+# 242. Valid Anagram
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_counter = collections.Counter(s)
+        t_counter = collections.Counter(t)
+        
+        return s_counter == t_counter
+        
