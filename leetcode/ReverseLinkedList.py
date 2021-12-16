db9c2c792bddbecf96eb41c00b3b647374873961
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2021-12-16 12:37:05 +0530

    #206

--- leetcode/ReverseLinkedList.py
+++ leetcode/ReverseLinkedList.py
@@ -7,13 +7,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         previous = None
-        while head:
-            current = head
-            head = head.next
-            current.next = previous
-            previous = current
+        current = head        
+        while current:
+            temp = current.next # store reference to later elements in temp
+            current.next = previous # update the .next with previous element. first value will be None as the tail of LL is None.
+            previous = current # current element becomes the previous element for the next element in the LL
+            current = temp # get back the reference to the remaining elements. 
         return previous
             
 ################################################
